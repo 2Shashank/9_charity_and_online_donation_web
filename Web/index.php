804e999a7ac3d4817d04fa20fc912d9ea8a90c87
RohithVedantam <135631467+RohithVedantam@users.noreply.github.com>
2023-11-21 10:39:02 +0530

    Update and rename index.html to index.php

---
+++ Web/index.php
@@ -0,0 +1,88 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Charity and Online Donation web</title>
+    <!-- Meta tags for responsive website -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Icons -->
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- External CSS -->
+    <link rel="stylesheet" href="index_style.css">
+    <link rel="stylesheet" href="footer.css">
+    <script src="https://kit.fontawesome.com/1165876da6.js" crossorigin="anonymous" ></script>
+    <!-- Chart.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+    <!-- Custom CSS -->
+
+</head>
+<body>
+    <!-- Navigation bar -->
+    <div class="topnav">
+        <a class="active" href="#"><i class="fa fa-home"></i> Home</a>
+        <a href="contact.html"><i class="fa fa-envelope"></i> Contact</a>
+        <a href="login.php"><i class="fa fa-user"></i> Log in</a>
+        <a href="admin_login.php"><i class="fa fa-user"></i>Admin Login</a>
+    </div>
+    </br>
+    </br>
+    </br>
+
+    <!-- Social media bar -->
+    <div class="icon-bar">
+        <a href="https://www.facebook.com/" class="facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+        <a href="https://twitter.com/" class="twitter" target="_blank"><i class="fa fa-twitter"></i></a>
+        <a href="https://accounts.google.com/" class="google" target="_blank"><i class="fa fa-google-plus"></i></a>
+        <a href="https://gr.pinterest.com/" class="pinterest" target="_blank"><i class="fa fa-pinterest-p"></i></a>
+        <a href="https://www.instagram.com" class="instagram" target="_blank"><i class="fa fa-instagram"></i></a>
+    </div>
+
+    <!-- Main text -->
+    <div class="main">
+        <h2>Who are we?</h2>
+        <p>In our charity, we provide help to people in need. People impacted by poverty, starvation, natural disasters, and war are some of the people we help every day. Through our help, we encourage them to dream of a better life. We support, financially and physically, human rights for people of all ages around the world.</p>
+        <br /><br />
+        <div id="div3-el">
+            <a href="login.php"><img src="https://m.economictimes.com/thumb/msid-63293846,width-1200,height-900,resizemode-4,imgsize-49958/donation-charity.jpg" width="1150"></a>
+        </div>
+        <h2>Want to help?</h2>
+        <p>Become a member! Your donation could be life-saving for someone else around the world. Your donation can help us provide millions of people a better life. <b>Get involved.</b> Take action with us. Join our team for some voluntary actions of our charity.</br></br></p>
+        <p align="center"><b>Together we are stronger.</b> </br></br> <i class="fas fa-hand-holding-heart"></i> </p></br>
+    </div>
+
+    <!-- Footer section  -->
+    <footer>
+      <div class="container">
+        <div class="footer-content">
+          <h3>Contact Us</h3>
+          <p>Email:charityweb12@gmail.com</p>
+          <p>Phone:9857425124</p>
+          <p>Address:PES university EC Bengaluru 560100</p>
+        </div>
+        <div class="footer-content">
+          <h3>Follow Us</h3>
+          <ul class="social-icons">
+            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="bottom-bar">
+        <p>“Giving is not just about making a donation. ..."</p>
+        <p>&copy; 2023. All rights reserved</p>
+      </div>
+    </footer>
+
+    
+    <!-- Bootstrap JS (Make sure to include Bootstrap JS files) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
