494e3e1860f9027beb7ae3ff9af3071a41104ed9
2Shashank <110035493+2Shashank@users.noreply.github.com>
2023-12-09 18:28:42 +0530

    Update and rename contact.html to contact.php

--- Web/contact.php
+++ Web/contact.php
@@ -1,3 +1,20 @@
+<?php 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$to = "nagarajam01011974@gmail.com";
+$subject = "Mail form Donation web";
+
+$headers = "From: " .$name. "\r\n" ;
+$txt = "You recieved message form :" .$name ."\r\nEmail:" .$email ."\r\nMessage: " . $message;
+$message = "Messaging from ..";
+if($email != NULL){
+    mail($to, $subject, $txt, $email);
+}
+header('Location: index.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
